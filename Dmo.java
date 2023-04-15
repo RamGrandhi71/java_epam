687409d0cb1bd7c152be32866199f46ceff4b26a
saicharankankati <84198707+saicharankankati@users.noreply.github.com>
2023-04-15 14:14:55 +0530

    print spelling is wrong

--- Dmo.java
+++ Dmo.java
@@ -3,8 +3,8 @@ public class Dmo {
     {
         B ob=new B();
         ob.run();
-        ob.prnt(9);
-        ob.prnt(3,4);
+        ob.print(9);
+        ob.print(3,4);
 //        int res=ob.prnt(8,7);
         //System.out.println(res);
         A obj=new A();
